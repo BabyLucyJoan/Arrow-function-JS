dfa1c0df5304575ffebc132e266b5e903d07641c
BabyLucyJoan <nwokoyelucy@gmail.com>
2023-10-10 19:39:04 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+
+
+const myself = ()=>{
+const isPaymentMade = true
+age = 22
+myName = "Lucy"
+console.log(isPaymentMade, age, myName);
+}
+
+myself()
+
+
+if (isPaymentMade = true && age>18 && myName.includes("A")){console.log("Grade A")}
+else if (isPaymentMade = true && age>18 || myName.includes("A")){console.log("Grade B")}
+else (isPaymentMade = true || age>18 && myName.includes("A"));{console.log("Grade C")} 
